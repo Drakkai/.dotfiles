5862b8f4a30724faedf46b3f96c6181fcb76b843
drakkai <drakkai@outlook.com>
2025-04-06 22:56:08 +0200

    2025-04-06T22:56:08

--- .oh-my-zsh-custom/aliases.zsh
+++ .oh-my-zsh-custom/aliases.zsh
@@ -4,4 +4,4 @@ elif command -v dnf > /dev/null ; then
   alias update="sudo dnf update -y"
 fi
 
-alias yadm_push="yadm commit -a -m \"$(date +\"%Y-%m-%dT%T\")\" && yadm push origin" 
+alias yadm_push='yadm commit -a -m "$(date +%Y-%m-%dT%T)" && yadm push origin'
